af1b6c282758028eba3e9230fc0cdd6c1f54256e
Maxim Babichev <maksim.babichev95@gmail.com>
2022-02-16 22:58:55 +0300

    fix

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/dustin/go-humanize"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"sync"
 )
@@ -25,23 +27,27 @@ func fastSize(path string, info os.FileInfo) (uint64, error) {
 	var errSize error = nil
 	var wg sync.WaitGroup
 	for _, file := range files {
-		if file.IsDir() {
-			wg.Add(1)
-
-			go func(f os.FileInfo) {
-				defer wg.Done()
-				res, err := fastSize(newPath, f)
-				if err != nil {
-					errSize = err
-				}
-
-				size += res
-			}(file)
+		if !file.IsDir() {
+			size += uint64(file.Size())
 
 			continue
 		}
 
-		size += uint64(file.Size())
+		wg.Add(1)
+
+		go func(f os.FileInfo) {
+			defer wg.Done()
+
+			err := filepath.Walk(newPath+"/"+f.Name(),
+				func(_ string, fw fs.FileInfo, err error) error {
+					size += uint64(fw.Size())
+					return err
+				})
+
+			if err != nil {
+				errSize = err
+			}
+		}(file)
 	}
 
 	wg.Wait()
@@ -67,7 +73,7 @@ func main() {
 				log.Fatal(err)
 			}
 
-			fmt.Printf("%6s %12s\n", strings.ReplaceAll(humanize.Bytes(bytes), " ", ""), f.Name())
+			fmt.Printf("%-7s %s\n", strings.ReplaceAll(humanize.Bytes(bytes), " ", ""), f.Name())
 		}(file)
 	}
 
