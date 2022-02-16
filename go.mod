ef517bb4aceb395e4d9393261e5419e08cb04ce3
Maxim Babichev <maksim.babichev95@gmail.com>
2022-02-16 22:20:10 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bavix/dius
+
+go 1.17
+
+require github.com/dustin/go-humanize v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"sync"
+)
+
+func fastSize(path string, info os.FileInfo) (uint64, error) {
+	if !info.IsDir() {
+		return uint64(info.Size()), nil
+	}
+
+	newPath := path + "/" + info.Name()
+	files, err := ioutil.ReadDir(newPath)
+	if err != nil {
+		return 0, err
+	}
+
+	var size uint64 = 0
+	var errSize error = nil
+	var wg sync.WaitGroup
+	for _, file := range files {
+		wg.Add(1)
+
+		go func(f os.FileInfo) {
+			defer wg.Done()
+			res, err := fastSize(newPath, f)
+			if err != nil {
+				errSize = err
+			}
+
+			size += res
+		}(file)
+
+	}
+
+	wg.Wait()
+
+	return size, errSize
+}
+
+func main() {
+	path, _ := os.Getwd()
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+	for _, file := range files {
+		wg.Add(1)
+
+		go func(f os.FileInfo) {
+			defer wg.Done()
+			bytes, err := fastSize(path, f)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%6s %12s\n", strings.ReplaceAll(humanize.Bytes(bytes), " ", ""), f.Name())
+		}(file)
+	}
+
+	wg.Wait()
+}
