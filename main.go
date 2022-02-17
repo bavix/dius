84084a59f4dfc515aa1d8659dbc918a8620d2d0b
Maxim Babichev <maksim.babichev95@gmail.com>
2022-02-17 22:22:24 +0300

    refactoring

--- main.go
+++ main.go
@@ -7,60 +7,32 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"runtime"
-	"sort"
+	"path/filepath"
 	"strings"
 	"sync"
-	"time"
 )
-
-type WaitGroupCustom struct {
-	wg    sync.WaitGroup
-	mutex sync.Mutex
-}
-
-func (c *WaitGroupCustom) Inc() {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	for runtime.NumGoroutine() > 1000 {
-		time.Sleep(time.Millisecond)
-	}
-
-	c.wg.Add(1)
-}
-
-func (c *WaitGroupCustom) Done() {
-	defer c.wg.Done()
-}
-
-func (c *WaitGroupCustom) Wait() {
-	defer c.wg.Wait()
-}
 
 func fastSize(path string, info os.FileInfo) (uint64, error) {
 	if !info.IsDir() {
 		return uint64(info.Size()), nil
 	}
 
-	newPath := path + "/" + info.Name()
+	newPath := filepath.Join(path, info.Name())
 	files, err := ioutil.ReadDir(newPath)
 	if err != nil {
 		return 0, err
 	}
 
-	var size uint64 = 0
 	var errSize error = nil
-	var wg WaitGroupCustom
+	var size uint64 = 0
+	var wg sync.WaitGroup
 	for _, file := range files {
 		if !file.IsDir() {
 			size += uint64(file.Size())
-
 			continue
 		}
 
-		wg.Inc()
-
+		wg.Add(1)
 		go func(f os.FileInfo) {
 			defer wg.Done()
 
@@ -80,33 +52,34 @@ func fastSize(path string, info os.FileInfo) (uint64, error) {
 
 func main() {
 	path, _ := os.Getwd()
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sort.SliceStable(files, func(i, j int) bool {
-		return strings.Compare(files[i].Name(), files[j].Name()) == -1
-	})
-
-	var wg WaitGroupCustom
+	var wg sync.WaitGroup
 	for _, file := range files {
-		wg.Inc()
+		wg.Add(1)
 
 		go func(f os.FileInfo) {
 			defer wg.Done()
 			bytes, err := fastSize(path, f)
-			if err == nil {
-				line := fmt.Sprintf("%-7s %s\n", strings.ReplaceAll(humanize.Bytes(bytes), " ", ""), f.Name())
-				if f.IsDir() {
-					color.Blue(line)
-				} else if strings.HasPrefix(f.Name(), ".") {
-					color.Cyan(line)
-				} else {
-					color.White(line)
-				}
-			} else {
+			if err != nil {
 				color.Red(err.Error())
+				return
+			}
+
+			line := fmt.Sprintf("%-7s %s\n", strings.ReplaceAll(humanize.Bytes(bytes), " ", ""), f.Name())
+			if f.IsDir() {
+				color.Blue(line)
+			} else if strings.HasPrefix(f.Name(), ".") {
+				color.Cyan(line)
+			} else {
+				color.White(line)
 			}
 		}(file)
 	}
